724859d55ff71029cd796c76e7fb651600ef61db
manchann <chl8273@naver.com>
2021-10-22 14:41:01 +0900

    change path

--- lambda_bulid.sh
+++ lambda_bulid.sh
@@ -4,10 +4,10 @@ docker build -t jg-lambda-image-container . --no-cache
 
 export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 
-docker tag jg-lambda-image-container $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/jg-lambda-image-container
+docker tag jg-lambda-ensemble-image $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/jg-lambda-ensemble-image
 
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com
 
-docker push $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/jg-lambda-image-container
+docker push $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/jg-lambda-ensemble-image
 
 docker rmi -f $(docker images -q)
